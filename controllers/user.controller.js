c394dd48ad8655fea01f977934ddbb6d3a47650c
Tran Dao Quang Chuong <chuongcongtu9x@gmail.com>
2020-04-24 16:07:56 +0700

    design

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,6 +11,7 @@ module.exports.index = (req, res) => {
 module.exports.create = (req, res) => {
     req.body.id = shortid.generate();
 
+    req.body.avatar = req.file.path.split('/').slice(1).join('/');
     db.get('users').push(req.body).write();
 
     res.redirect('/users');
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+var multer = require('multer');
 
 const controller = require('../controllers/user.controller');
 var validate = require('../validate/user.validate');
+
+var upload = multer({ dest: './public/uploads/' });
 
 router.get("/", controller.index);
 
@@ -14,6 +17,6 @@ router.get("/:id/delete", controller.delete);
 
 router.get("/search", controller.search);
 
-router.post("/create", validate.postCreate, controller.create);
+router.post("/create", upload.single('avatar'), validate.postCreate, controller.create);
 
 module.exports = router;
